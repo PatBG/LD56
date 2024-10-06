b2489709efdfdb3caa956d66519e1e56c581e7f4
PatBG <pat@patbgames.com>
2024-10-06 11:53:55 +0200

    Management of CreatureState : Hungry / Satiated / Sleepy

--- src/components/Creature.ts
+++ src/components/Creature.ts
@@ -3,26 +3,39 @@ interface IsInRange {
     (x: number, y: number): boolean;
 }
 
+export enum CreatureState {
+    Hungry = 0,
+    Satiated = 2,
+    Sleepy = 4,
+}
+
 export class Creature {
     sprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    creatureState: CreatureState;
+    coinCollider: Phaser.Physics.Arcade.Collider;
 
     constructor(scene: Phaser.Scene, groundLayer: Phaser.Tilemaps.TilemapLayer, x: number, y: number) {
-        // create the player sprite    
+        this.creatureState = CreatureState.Hungry;
         this.sprite = scene.physics.add.sprite(x, y, 'creature');
-        this.sprite.setBounce(0.2); // our player will bounce from items
-        this.sprite.setCollideWorldBounds(true); // don't go out of the map
-        scene.physics.add.collider(groundLayer, this.sprite); // ensure groundLayer is passed as an argument
+        this.sprite.setBounce(0.2);
+        this.sprite.setCollideWorldBounds(true);                    // don't go out of the map
+        scene.physics.add.collider(groundLayer, this.sprite);       // ensure groundLayer is passed as an argument
     }
 
     update(isAction: boolean, isInRangeCallback: IsInRange, x: number, y: number) {
         const isInRange = isInRangeCallback(this.sprite.x, this.sprite.y);
-        this.sprite.setFrame(isInRange ? 1 : 0);
+        if (this.creatureState != CreatureState.Sleepy) {
+            this.sprite.setFrame(this.creatureState.valueOf() + (isInRange ? 1 : 0));
+        }
         // If the creature is on the ground
         if (this.sprite.body.onFloor()) {
             const idle_jump = 200 + Phaser.Math.Between(50, 0);
-            const magic_number_y = 2;
-            const delta_top_of_target = -100;
-            if (isInRange && isAction) {
+            if (this.creatureState == CreatureState.Sleepy) {
+                this.sprite.body.setVelocity(0, -(idle_jump / 2));            // jump up half the distance
+            }
+            else if (isInRange && isAction) {
+                const magic_number_y = 2;
+                const delta_top_of_target = -100;
                 const action_jump = 550 + Phaser.Math.Between(50, 0);
                 const delta_x = x - this.sprite.x;
                 const delta_y = (y + delta_top_of_target) - this.sprite.y;
@@ -38,7 +51,7 @@ export class Creature {
             }
         }
         else {
-            if (isInRange && isAction) {
+            if (isInRange && isAction && this.creatureState != CreatureState.Sleepy) {
                 // TODO: Implement Air control ?
             }
         }
